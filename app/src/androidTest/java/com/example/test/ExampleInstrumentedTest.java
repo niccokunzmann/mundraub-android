748401d85c7e37aa3a4ddcf7d942d2c4b96f37b5
Nicco Kunzmann <niccokunzmann@rambler.ru>
2018-07-31 20:45:17 +0200

    rename project

--- app/src/androidTest/java/com/example/test/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/test/ExampleInstrumentedTest.java
@@ -19,6 +19,6 @@ import static org.junit.Assert.*;
     // Context of the app under test.
     Context appContext = InstrumentationRegistry.getTargetContext();
 
-    assertEquals("com.rilixtech.test", appContext.getPackageName());
+    assertEquals("eu.quelltext.mundraub", appContext.getPackageName());
   }
 }
--- app/src/main/java/eu/quelltext/mundraub/MainActivity.java
+++ app/src/main/java/eu/quelltext/mundraub/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.test;
+package eu.quelltext.mundraub;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import eu.quelltext.mundraub.R;
 
 public class MainActivity extends AppCompatActivity {
 
