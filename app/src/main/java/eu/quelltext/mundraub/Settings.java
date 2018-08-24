9c35e4e09b0b6eef23fb6477fe1e158f779d8c73
Travis-CI <mundraub-android@travis-ci.org>
2018-08-24 21:39:56 +0000

    v1.11: set the commit hash

--- app/src/main/java/eu/quelltext/mundraub/Settings.java
+++ app/src/main/java/eu/quelltext/mundraub/Settings.java
@@ -5,7 +5,7 @@ import eu.quelltext.mundraub.error.Logger;
 public class Settings {
 
     public static final String INVALID_HASH = "0000000000000000000000000000000000000000";
-    public static final String COMMIT_HASH = INVALID_HASH;
+    public static final String COMMIT_HASH = "48c5f0d3266ae02f9b6b92ec906c0f742b6343d0";
 
     public static String getShortHash(){
         return COMMIT_HASH.substring(0, 7);
