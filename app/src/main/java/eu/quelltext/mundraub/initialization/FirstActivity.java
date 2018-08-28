c5a0be820e7902177276794dfbf6aa69727c8651
Nicco Kunzmann <niccokunzmann@rambler.ru>
2018-08-29 00:45:19 +0200

    ask only for permissions when required
    - related 8a59648a6035e2a0e32d7e29112d44256cfc54f3
    - fixes https://github.com/niccokunzmann/mundraub-android/issues/72

--- app/src/main/java/eu/quelltext/mundraub/initialization/FirstActivity.java
+++ app/src/main/java/eu/quelltext/mundraub/initialization/FirstActivity.java
@@ -3,12 +3,4 @@ package eu.quelltext.mundraub.initialization;
 import eu.quelltext.mundraub.error.MundraubBaseActivity;
 
 public class FirstActivity extends MundraubBaseActivity {
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        // TODO remove and ask when needed.
-        getPermissions().checkAllPermissions();
-    }
-
 }
