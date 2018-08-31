ff071cce375976c08149cbd031386bd2cdfbc159
Nicco Kunzmann <niccokunzmann@rambler.ru>
2018-08-31 09:11:40 +0200

    add compatibility AArray.includes

---
+++ app/src/main/assets/map/compatibility.js
@@ -0,0 +1,12 @@
+// Compatibility functions
+
+if (!Array.prototype.includes) {
+    Array.prototype.includes = function (element) {
+        for (var i = 0; i < this.length; i++) {
+            if (this[i] == element) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
