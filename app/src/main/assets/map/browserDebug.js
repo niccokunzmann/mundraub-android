40c161dd65490ec679e7885286d5d15f5e7f9b86
Nicco Kunzmann <niccokunzmann@rambler.ru>
2018-08-31 09:10:26 +0200

    debug print browser version

---
+++ app/src/main/assets/map/browserDebug.js
@@ -0,0 +1,5 @@
+// from http://www.alanwood.net/demos/browserinfo.html
+// debug the browser version
+
+console.log("-------------- Browser -------------- ");
+console.log("navigator.userAgent: " + navigator.userAgent);
