654ed1945f37a4c972263806ae4806a453660605
Nicco Kunzmann <niccokunzmann@rambler.ru>
2018-09-03 19:21:01 +0200

    place text on top of the group on map

--- app/src/main/assets/map/icons.js
+++ app/src/main/assets/map/icons.js
@@ -24,7 +24,7 @@ function iconFromName(name, type) {
 function setIconDescriptionOfMarker(marker, text) {
     var div = marker.icon.imageDiv.appendChild(document.createElement("div"));
     div.innerText = text;
-    div.class = "marker-description";
+    div.className = "marker-description";
 }
 
 var categoryToName = {
