0d42ca31da18d1177e8a63d32c39a7b53dded02f
Nicco Kunzmann <niccokunzmann@rambler.ru>
2018-09-06 23:20:53 +0200

    set all control blocks automatically from map
    - improves https://github.com/niccokunzmann/mundraub-android/issues/99

--- app/src/main/assets/map/controls.js
+++ app/src/main/assets/map/controls.js
@@ -1,13 +1,11 @@
 
-function blockMapClick(elementId) {
-    var element = document.getElementById(elementId);
-    element.onclick = function(event) {
-        event.stopPropagation();
-    }
-}
-
 function controlsBlockMapClick() {
-    blockMapClick("OpenLayers_Control_Zoom_322");
-    blockMapClick("OpenLayers_Control_LayerSwitcher_323");
+    map.controls.forEach(function (control) {
+        if (control.div) {
+            control.div.onclick = function(event) {
+                event.stopPropagation();
+            }
+        }
+    });
 }
 
