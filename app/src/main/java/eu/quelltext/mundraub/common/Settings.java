4bb10cbf88daacc55df741487f83b1c2625f219c
Travis-CI <mundraub-android@travis-ci.org>
2018-09-06 16:06:07 +0000

    Version v1.50
    - c4b2583 create issue summary of release
        - fixes https://github.com/niccokunzmann/mundraub-android/issues/100
    - cfce861 remove old obsolete file
        - related: https://github.com/niccokunzmann/mundraub-android/issues/100

--- app/src/main/java/eu/quelltext/mundraub/common/Settings.java
+++ app/src/main/java/eu/quelltext/mundraub/common/Settings.java
@@ -39,7 +39,7 @@ public class Settings {
 
     private static final String PLANT_STORAGE_DIRECTORY_NAME = "eu.quelltext.mundraub";
     public static final String INVALID_HASH = "0000000000000000000000000000000000000000";
-    public static final String COMMIT_HASH = INVALID_HASH;
+    public static final String COMMIT_HASH = "0ce8155683753fe0aa1fe19fc4a48e08911c398f";
     private static final String PERMISSION_PREFIX = "askForPermission_";
     public static int COMMIT_SUCCESSFUL = ChangeListener.SETTINGS_CAN_CHANGE;
 
