88b4779ccc3ed6e4897875e50bc773590fe072f3
Travis-CI <mundraub-android@travis-ci.org>
2018-09-08 20:46:30 +0000

    Version v1.54
    - f11613a query all plants
        - fixes https://github.com/niccokunzmann/mundraub-android/issues/96
    - b9ced85 fix recursion error
        - see https://github.com/niccokunzmann/mundraub-android/issues/96#issuecomment-419628258

--- app/src/main/java/eu/quelltext/mundraub/common/Settings.java
+++ app/src/main/java/eu/quelltext/mundraub/common/Settings.java
@@ -39,7 +39,7 @@ public class Settings {
 
     private static final String PLANT_STORAGE_DIRECTORY_NAME = "eu.quelltext.mundraub";
     public static final String INVALID_HASH = "0000000000000000000000000000000000000000";
-    public static final String COMMIT_HASH = INVALID_HASH;
+    public static final String COMMIT_HASH = "d02f8c73be61fb5591f1df813cccff2333f9ed48";
     private static final String PERMISSION_PREFIX = "askForPermission_";
     public static int COMMIT_SUCCESSFUL = ChangeListener.SETTINGS_CAN_CHANGE;
 
