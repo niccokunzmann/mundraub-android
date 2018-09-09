f9e7aafb8187fdc0be483e358691d710196858a6
Nicco Kunzmann <niccokunzmann@rambler.ru>
2018-09-09 08:11:20 +0200

    test compilation of the local proxy with Travis

--- scripts/runHTTPProxy.sh
+++ scripts/runHTTPProxy.sh
@@ -4,7 +4,11 @@ set -e
 
 cd "`dirname \"$0\"`"
 
-PORT="$1"
+if [ "$1" == "--compile" ]; then
+    SKIP_RUN="true"
+else
+    SKIP_RUN="false"
+fi
 
 cd ..
 
@@ -23,6 +27,8 @@ javac eu/quelltext/mundraub/map/MundraubMapAPI.java
 #    wget -O- 'http://localhost:'"$PORT"'/plant?bbox=13.083043098449709,50.678268138692154,13.151235580444336,50.685827559768505&zoom=15&cat=4,5,6,7,8,9,10,11,12,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37'
 #) &
 
-java eu.quelltext.mundraub.map.MundraubMapAPI "$PORT"
+if [ "$SKIP_RUN" == "false" ]; then
+    java eu.quelltext.mundraub.map.MundraubMapAPI "$PORT"
+fi
 
 
