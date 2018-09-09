2ea3e097b0371d28e79024bec7b6e0a7cf94ebff
Travis-CI <mundraub-android@travis-ci.org>
2018-09-09 09:48:06 +0000

    Version v1.59
    - 0ac185e describe how to pass translations from the Android app to the JavaScript map
        - contributes to https://github.com/niccokunzmann/mundraub-android/issues/106

--- app/src/main/java/eu/quelltext/mundraub/common/Settings.java
+++ app/src/main/java/eu/quelltext/mundraub/common/Settings.java
@@ -39,7 +39,7 @@ public class Settings {
 
     private static final String PLANT_STORAGE_DIRECTORY_NAME = "eu.quelltext.mundraub";
     public static final String INVALID_HASH = "0000000000000000000000000000000000000000";
-    public static final String COMMIT_HASH = INVALID_HASH;
+    public static final String COMMIT_HASH = "7fbdf89d430e65680aacb86d397f4bedab3c3fe7";
     private static final String PERMISSION_PREFIX = "askForPermission_";
     public static int COMMIT_SUCCESSFUL = ChangeListener.SETTINGS_CAN_CHANGE;
 
