0f3b27c1daf0723c98477007c7cda6c4e55975d1
Travis-CI <mundraub-android@travis-ci.org>
2018-09-09 08:13:03 +0000

    Version v1.55
    - 0445f0b create plant marker with pop-up
        - contributes to https://github.com/niccokunzmann/mundraub-android/issues/106

--- app/src/main/java/eu/quelltext/mundraub/common/Settings.java
+++ app/src/main/java/eu/quelltext/mundraub/common/Settings.java
@@ -39,7 +39,7 @@ public class Settings {
 
     private static final String PLANT_STORAGE_DIRECTORY_NAME = "eu.quelltext.mundraub";
     public static final String INVALID_HASH = "0000000000000000000000000000000000000000";
-    public static final String COMMIT_HASH = INVALID_HASH;
+    public static final String COMMIT_HASH = "77560f503ba0bfd1d644ca470541aed3b60a1f3e";
     private static final String PERMISSION_PREFIX = "askForPermission_";
     public static int COMMIT_SUCCESSFUL = ChangeListener.SETTINGS_CAN_CHANGE;
 
