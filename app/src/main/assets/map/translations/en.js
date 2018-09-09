5f0032769039bb47b45c4a2b9e7cdf44cb8bc851
Nicco Kunzmann <niccokunzmann@rambler.ru>
2018-09-09 13:06:32 +0200

    add translation of distance to marker
    - contributes to https://github.com/niccokunzmann/mundraub-android/issues/107

--- app/src/main/assets/map/translations/en.js
+++ app/src/main/assets/map/translations/en.js
@@ -2,6 +2,7 @@ define({
     "Location": "Location",
     "Plants": "Plants",
     "Satellite": "Satellite",
-    "GPS": "GPS"
+    "GPS": "GPS",
+    "Distance to marker": "Distance to marker"
 }); // do not put a comma at the end of the last translation
 
