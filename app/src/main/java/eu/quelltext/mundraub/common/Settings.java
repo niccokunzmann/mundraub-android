641ec20b6c9c878e4d89bcca53798f252576a9aa
Travis-CI <mundraub-android@travis-ci.org>
2018-09-13 14:30:51 +0000

    Version v1.69
    - 68eb03b use app categories or mundraub tid on the map
        - fixes https://github.com/niccokunzmann/mundraub-android/issues/115

--- app/src/main/java/eu/quelltext/mundraub/common/Settings.java
+++ app/src/main/java/eu/quelltext/mundraub/common/Settings.java
@@ -39,7 +39,7 @@ public class Settings {
 
     private static final String PLANT_STORAGE_DIRECTORY_NAME = "eu.quelltext.mundraub";
     public static final String INVALID_HASH = "0000000000000000000000000000000000000000";
-    public static final String COMMIT_HASH = INVALID_HASH;
+    public static final String COMMIT_HASH = "892ea79cccd4fbc89f3b86ca65f98a510e28ebba";
     private static final String PERMISSION_PREFIX = "askForPermission_";
     public static int COMMIT_SUCCESSFUL = ChangeListener.SETTINGS_CAN_CHANGE;
 
