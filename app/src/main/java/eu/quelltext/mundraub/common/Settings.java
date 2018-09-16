b33222d6eaf3b141b0a8cd21a37e29b3d84b4ef0
Travis-CI <mundraub-android@travis-ci.org>
2018-09-16 14:27:00 +0000

    Version v1.84
    - 8f4c5dc save credentials per API
        - fixes https://github.com/niccokunzmann/mundraub-android/issues/129

--- app/src/main/java/eu/quelltext/mundraub/common/Settings.java
+++ app/src/main/java/eu/quelltext/mundraub/common/Settings.java
@@ -40,7 +40,7 @@ public class Settings {
 
     private static final String PLANT_STORAGE_DIRECTORY_NAME = "eu.quelltext.mundraub";
     public static final String INVALID_HASH = "0000000000000000000000000000000000000000";
-    public static final String COMMIT_HASH = INVALID_HASH;
+    public static final String COMMIT_HASH = "33edbf8eaa66f03477d6fea986deb07e3f768cf4";
     private static final String PERMISSION_PREFIX = "askForPermission_";
     public static int COMMIT_SUCCESSFUL = ChangeListener.SETTINGS_CAN_CHANGE;
 
