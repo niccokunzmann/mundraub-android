bbab23b7ff310ab0944e905c641f0331e1159b4b
Travis-CI <mundraub-android@travis-ci.org>
2018-09-17 18:26:45 +0000

    Version v1.86
    - 145af43 dialogs can be displayed when an activity is created, not when it is started
        - fixes https://github.com/niccokunzmann/mundraub-android/issues/136

--- app/src/main/java/eu/quelltext/mundraub/common/Settings.java
+++ app/src/main/java/eu/quelltext/mundraub/common/Settings.java
@@ -40,7 +40,7 @@ public class Settings {
 
     private static final String PLANT_STORAGE_DIRECTORY_NAME = "eu.quelltext.mundraub";
     public static final String INVALID_HASH = "0000000000000000000000000000000000000000";
-    public static final String COMMIT_HASH = INVALID_HASH;
+    public static final String COMMIT_HASH = "38cda45b3b4bf888496656c5b08174ef91fa5f83";
     private static final String PERMISSION_PREFIX = "askForPermission_";
     public static int COMMIT_SUCCESSFUL = ChangeListener.SETTINGS_CAN_CHANGE;
 
