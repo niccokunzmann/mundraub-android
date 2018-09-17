d485db16a97633a239635a4da0eed4ee49e40c62
Travis-CI <mundraub-android@travis-ci.org>
2018-09-17 18:59:48 +0000

    Version v1.89
    - 4e2d60e describe na-ovoce as provider
        - fixes https://github.com/niccokunzmann/mundraub-android/issues/134

--- app/src/main/java/eu/quelltext/mundraub/common/Settings.java
+++ app/src/main/java/eu/quelltext/mundraub/common/Settings.java
@@ -40,7 +40,7 @@ public class Settings {
 
     private static final String PLANT_STORAGE_DIRECTORY_NAME = "eu.quelltext.mundraub";
     public static final String INVALID_HASH = "0000000000000000000000000000000000000000";
-    public static final String COMMIT_HASH = INVALID_HASH;
+    public static final String COMMIT_HASH = "23dea60242a4855c4a5b4b4caf425579180ea3a9";
     private static final String PERMISSION_PREFIX = "askForPermission_";
     public static int COMMIT_SUCCESSFUL = ChangeListener.SETTINGS_CAN_CHANGE;
 
