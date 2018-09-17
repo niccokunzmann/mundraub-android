fa2b0407dad046dbb66bbcdf448e538d9f867b79
Travis-CI <mundraub-android@travis-ci.org>
2018-09-17 18:54:53 +0000

    Version v1.88
    - 2e8f794 Display Mundraub and Na-Ovoce side by side
        - contributes to https://github.com/niccokunzmann/mundraub-android/issues/134

--- app/src/main/java/eu/quelltext/mundraub/common/Settings.java
+++ app/src/main/java/eu/quelltext/mundraub/common/Settings.java
@@ -40,7 +40,7 @@ public class Settings {
 
     private static final String PLANT_STORAGE_DIRECTORY_NAME = "eu.quelltext.mundraub";
     public static final String INVALID_HASH = "0000000000000000000000000000000000000000";
-    public static final String COMMIT_HASH = INVALID_HASH;
+    public static final String COMMIT_HASH = "be1a88b2b6ba2992876aab2746a993c637f4239c";
     private static final String PERMISSION_PREFIX = "askForPermission_";
     public static int COMMIT_SUCCESSFUL = ChangeListener.SETTINGS_CAN_CHANGE;
 
