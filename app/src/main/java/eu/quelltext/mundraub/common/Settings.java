45be0cf53b2ce10382991c20f778e11838960ddc
Travis-CI <mundraub-android@travis-ci.org>
2018-09-17 18:40:32 +0000

    Version v1.87
    - d1aafab load position from its type
        - fixes https://github.com/niccokunzmann/mundraub-android/issues/135

--- app/src/main/java/eu/quelltext/mundraub/common/Settings.java
+++ app/src/main/java/eu/quelltext/mundraub/common/Settings.java
@@ -40,7 +40,7 @@ public class Settings {
 
     private static final String PLANT_STORAGE_DIRECTORY_NAME = "eu.quelltext.mundraub";
     public static final String INVALID_HASH = "0000000000000000000000000000000000000000";
-    public static final String COMMIT_HASH = INVALID_HASH;
+    public static final String COMMIT_HASH = "9349b07e89d42009b0f6dc9f4c00e2ad1230f42f";
     private static final String PERMISSION_PREFIX = "askForPermission_";
     public static int COMMIT_SUCCESSFUL = ChangeListener.SETTINGS_CAN_CHANGE;
 
