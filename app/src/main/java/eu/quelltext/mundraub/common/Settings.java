4fff7d1c94fd87eb4a6c7403f93d4d9e059875f3
Travis-CI <mundraub-android@travis-ci.org>
2018-09-20 22:40:03 +0000

    Version v1.96
    - b420658 add fruit radar
        - contributes to https://github.com/niccokunzmann/mundraub-android/issues/131

--- app/src/main/java/eu/quelltext/mundraub/common/Settings.java
+++ app/src/main/java/eu/quelltext/mundraub/common/Settings.java
@@ -53,7 +53,7 @@ public class Settings {
 
     private static final String PLANT_STORAGE_DIRECTORY_NAME = "eu.quelltext.mundraub";
     public static final String INVALID_HASH = "0000000000000000000000000000000000000000";
-    public static final String COMMIT_HASH = INVALID_HASH;
+    public static final String COMMIT_HASH = "f642462bff3dd94b19bf299e8e8465796db3e34c";
     private static final String PERMISSION_PREFIX = "askForPermission_";
     public static int COMMIT_SUCCESSFUL = ChangeListener.SETTINGS_CAN_CHANGE;
 
