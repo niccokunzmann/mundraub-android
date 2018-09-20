5026b57b2cec06bb1b06cd9f474ab0bbacd273e3
Travis-CI <mundraub-android@travis-ci.org>
2018-09-20 12:15:32 +0000

    Version v1.92
    - addba07 ask if plant category changes before upload
        - fixes https://github.com/niccokunzmann/mundraub-android/issues/142

--- app/src/main/java/eu/quelltext/mundraub/common/Settings.java
+++ app/src/main/java/eu/quelltext/mundraub/common/Settings.java
@@ -53,7 +53,7 @@ public class Settings {
 
     private static final String PLANT_STORAGE_DIRECTORY_NAME = "eu.quelltext.mundraub";
     public static final String INVALID_HASH = "0000000000000000000000000000000000000000";
-    public static final String COMMIT_HASH = INVALID_HASH;
+    public static final String COMMIT_HASH = "f911b87c32dafff6f8f77c3bae965f84ed49dfc9";
     private static final String PERMISSION_PREFIX = "askForPermission_";
     public static int COMMIT_SUCCESSFUL = ChangeListener.SETTINGS_CAN_CHANGE;
 
