8beb7c520a8e67c0f0eaa5673fd2eb36c748d28a
Travis-CI <mundraub-android@travis-ci.org>
2018-09-21 09:51:43 +0000

    Version v1.98
    - ce553c7 remove newlines from description
        - fixes https://github.com/niccokunzmann/mundraub-android/issues/149

--- app/src/main/java/eu/quelltext/mundraub/common/Settings.java
+++ app/src/main/java/eu/quelltext/mundraub/common/Settings.java
@@ -53,7 +53,7 @@ public class Settings {
 
     private static final String PLANT_STORAGE_DIRECTORY_NAME = "eu.quelltext.mundraub";
     public static final String INVALID_HASH = "0000000000000000000000000000000000000000";
-    public static final String COMMIT_HASH = INVALID_HASH;
+    public static final String COMMIT_HASH = "2c087dfe72cae9c4d612a3fb176d60cf22b07892";
     private static final String PERMISSION_PREFIX = "askForPermission_";
     public static int COMMIT_SUCCESSFUL = ChangeListener.SETTINGS_CAN_CHANGE;
 
