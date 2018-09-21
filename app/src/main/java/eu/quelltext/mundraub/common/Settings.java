53aa733f5f2db85b4ac0f386c183488df824e3a7
Travis-CI <mundraub-android@travis-ci.org>
2018-09-21 12:43:48 +0000

    Version v1.99
    - b096b53 fix bug: plants not showing up in radar
        - contributes to https://github.com/niccokunzmann/mundraub-android/issues/131

--- app/src/main/java/eu/quelltext/mundraub/common/Settings.java
+++ app/src/main/java/eu/quelltext/mundraub/common/Settings.java
@@ -53,7 +53,7 @@ public class Settings {
 
     private static final String PLANT_STORAGE_DIRECTORY_NAME = "eu.quelltext.mundraub";
     public static final String INVALID_HASH = "0000000000000000000000000000000000000000";
-    public static final String COMMIT_HASH = INVALID_HASH;
+    public static final String COMMIT_HASH = "91b3f3bc4d40ca7290a0cbcafa81dbc83ac72c4c";
     private static final String PERMISSION_PREFIX = "askForPermission_";
     public static int COMMIT_SUCCESSFUL = ChangeListener.SETTINGS_CAN_CHANGE;
 
