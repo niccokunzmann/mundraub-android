b1d7818707fe2b2ebd43be95f764ab364901854f
Travis-CI <mundraub-android@travis-ci.org>
2018-09-23 22:41:40 +0000

    Version v1.106
    - 1a83131 create startup activity which loads plants
        - fixes https://github.com/niccokunzmann/mundraub-android/issues/156
        - fixes https://github.com/niccokunzmann/mundraub-android/issues/144
        - contributes to https://github.com/niccokunzmann/mundraub-android/issues/137
        - fixes https://github.com/niccokunzmann/mundraub-android/issues/104
        - adds place to display logo for https://github.com/niccokunzmann/mundraub-android/issues/101
        - closes https://github.com/niccokunzmann/mundraub-android/issues/78
        - fixes https://github.com/niccokunzmann/mundraub-android/issues/17

--- app/src/main/java/eu/quelltext/mundraub/common/Settings.java
+++ app/src/main/java/eu/quelltext/mundraub/common/Settings.java
@@ -53,7 +53,7 @@ public class Settings {
 
     private static final String PLANT_STORAGE_DIRECTORY_NAME = "eu.quelltext.mundraub";
     public static final String INVALID_HASH = "0000000000000000000000000000000000000000";
-    public static final String COMMIT_HASH = INVALID_HASH;
+    public static final String COMMIT_HASH = "cc972f6aec3d9123eed972bd120b2416ad7291ed";
     private static final String PERMISSION_PREFIX = "askForPermission_";
     public static int COMMIT_SUCCESSFUL = ChangeListener.SETTINGS_CAN_CHANGE;
 
