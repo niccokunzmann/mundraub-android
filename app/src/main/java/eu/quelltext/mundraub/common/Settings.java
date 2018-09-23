15e3146e71b734b510c9524eccaf9cbda1f8aa0d
Travis-CI <mundraub-android@travis-ci.org>
2018-09-23 23:18:21 +0000

    Version v1.107
    - cca618e show crash report when app opens
        - improves https://github.com/niccokunzmann/mundraub-android/issues/156

--- app/src/main/java/eu/quelltext/mundraub/common/Settings.java
+++ app/src/main/java/eu/quelltext/mundraub/common/Settings.java
@@ -53,7 +53,7 @@ public class Settings {
 
     private static final String PLANT_STORAGE_DIRECTORY_NAME = "eu.quelltext.mundraub";
     public static final String INVALID_HASH = "0000000000000000000000000000000000000000";
-    public static final String COMMIT_HASH = INVALID_HASH;
+    public static final String COMMIT_HASH = "0a1caa0aeb6c86991a9a19d5ec3462da75065362";
     private static final String PERMISSION_PREFIX = "askForPermission_";
     public static int COMMIT_SUCCESSFUL = ChangeListener.SETTINGS_CAN_CHANGE;
 
