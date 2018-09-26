a462daa332f41557d77272e7792db6c1177e7327
Travis-CI <mundraub-android@travis-ci.org>
2018-09-26 17:54:05 +0000

    Version v1.111
    - e136aef load map when translations are loaded
        - fixes https://github.com/niccokunzmann/mundraub-android/issues/161

--- app/src/main/java/eu/quelltext/mundraub/common/Settings.java
+++ app/src/main/java/eu/quelltext/mundraub/common/Settings.java
@@ -53,7 +53,7 @@ public class Settings {
 
     private static final String PLANT_STORAGE_DIRECTORY_NAME = "eu.quelltext.mundraub";
     public static final String INVALID_HASH = "0000000000000000000000000000000000000000";
-    public static final String COMMIT_HASH = INVALID_HASH;
+    public static final String COMMIT_HASH = "7c12f038660e356b8d14c3ddbed2c955617d685b";
     private static final String PERMISSION_PREFIX = "askForPermission_";
     public static int COMMIT_SUCCESSFUL = ChangeListener.SETTINGS_CAN_CHANGE;
 
