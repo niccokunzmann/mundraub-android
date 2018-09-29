4ba83d01d771e3f40868fbf2475495d8aa8b8886
Travis-CI <mundraub-android@travis-ci.org>
2018-09-29 06:57:39 +0000

    Version v1.116
    - 84eb3ac move phone screenshots into images folder
        - poposal https://github.com/niccokunzmann/mundraub-android/issues/69#issuecomment-425459894
        - contributes to https://github.com/niccokunzmann/mundraub-android/issues/69
    - b79473a choose which platforms to download markers from
        - contributes to https://github.com/niccokunzmann/mundraub-android/issues/155
        - fixes https://github.com/niccokunzmann/mundraub-android/issues/141

--- app/src/main/java/eu/quelltext/mundraub/common/Settings.java
+++ app/src/main/java/eu/quelltext/mundraub/common/Settings.java
@@ -52,7 +52,7 @@ public class Settings {
 
     private static final String PLANT_STORAGE_DIRECTORY_NAME = "eu.quelltext.mundraub";
     public static final String INVALID_HASH = "0000000000000000000000000000000000000000";
-    public static final String COMMIT_HASH = INVALID_HASH;
+    public static final String COMMIT_HASH = "2c32a504218ded40e3cbe775f19871f91325a9ef";
     private static final String PERMISSION_PREFIX = "askForPermission_";
     public static int COMMIT_SUCCESSFUL = ChangeListener.SETTINGS_CAN_CHANGE;
 
