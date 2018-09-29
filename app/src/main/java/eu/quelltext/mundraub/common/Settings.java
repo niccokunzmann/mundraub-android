d26020cf4f91a498476070c84865b486b296cff4
Travis-CI <mundraub-android@travis-ci.org>
2018-09-29 07:00:53 +0000

    Version v1.117
    - d4b3500 rename screen shot image path
        - contributes to https://github.com/niccokunzmann/mundraub-android/issues/69

--- app/src/main/java/eu/quelltext/mundraub/common/Settings.java
+++ app/src/main/java/eu/quelltext/mundraub/common/Settings.java
@@ -52,7 +52,7 @@ public class Settings {
 
     private static final String PLANT_STORAGE_DIRECTORY_NAME = "eu.quelltext.mundraub";
     public static final String INVALID_HASH = "0000000000000000000000000000000000000000";
-    public static final String COMMIT_HASH = INVALID_HASH;
+    public static final String COMMIT_HASH = "04ca5bb6524d5a63ed22ca6f9395bafb3460aae9";
     private static final String PERMISSION_PREFIX = "askForPermission_";
     public static int COMMIT_SUCCESSFUL = ChangeListener.SETTINGS_CAN_CHANGE;
 
