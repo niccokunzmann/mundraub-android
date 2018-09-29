e18656540bfe2be170e0cbe07dabd5880bf04f7f
Travis-CI <mundraub-android@travis-ci.org>
2018-09-29 13:34:24 +0000

    Version v1.118
    - 6eb0552 put more configuration into the browser url
        - contributes to https://github.com/niccokunzmann/mundraub-android/issues/164
        - contributes to https://github.com/niccokunzmann/mundraub-android/issues/111
    - d15bc7c update open layers
        - fixes https://github.com/niccokunzmann/mundraub-android/issues/88

--- app/src/main/java/eu/quelltext/mundraub/common/Settings.java
+++ app/src/main/java/eu/quelltext/mundraub/common/Settings.java
@@ -52,7 +52,7 @@ public class Settings {
 
     private static final String PLANT_STORAGE_DIRECTORY_NAME = "eu.quelltext.mundraub";
     public static final String INVALID_HASH = "0000000000000000000000000000000000000000";
-    public static final String COMMIT_HASH = INVALID_HASH;
+    public static final String COMMIT_HASH = "b35b738ff142b087c7fee7df0279d6f90e82e199";
     private static final String PERMISSION_PREFIX = "askForPermission_";
     public static int COMMIT_SUCCESSFUL = ChangeListener.SETTINGS_CAN_CHANGE;
 
