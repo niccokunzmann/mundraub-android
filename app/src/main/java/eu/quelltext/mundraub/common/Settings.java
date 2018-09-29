2e63f299fbea31d2d830b82765b960fdbe49852e
Travis-CI <mundraub-android@travis-ci.org>
2018-09-29 16:04:35 +0000

    Version v1.119
    - 00bf6b9 direct GPS position to app
        - fixes https://github.com/niccokunzmann/mundraub-android/issues/150
    - a9654a5 test the map url
        - contributes to https://github.com/niccokunzmann/mundraub-android/issues/150
        - contributes to https://github.com/niccokunzmann/mundraub-android/issues/111
    - 517a055 add browser GPS parameter
        - contributes to https://github.com/niccokunzmann/mundraub-android/issues/150
    - 577dbd4 export layer urls to settings
        - contributes to https://github.com/niccokunzmann/mundraub-android/issues/111

--- app/src/main/java/eu/quelltext/mundraub/common/Settings.java
+++ app/src/main/java/eu/quelltext/mundraub/common/Settings.java
@@ -52,7 +52,7 @@ public class Settings {
 
     private static final String PLANT_STORAGE_DIRECTORY_NAME = "eu.quelltext.mundraub";
     public static final String INVALID_HASH = "0000000000000000000000000000000000000000";
-    public static final String COMMIT_HASH = INVALID_HASH;
+    public static final String COMMIT_HASH = "b8fc2411db725d554c4f173a62fff10456359f33";
     private static final String PERMISSION_PREFIX = "askForPermission_";
     public static int COMMIT_SUCCESSFUL = ChangeListener.SETTINGS_CAN_CHANGE;
 
