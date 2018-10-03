8e54e65dd8d765382fc0a4e8a34397713740e84d
Nicco Kunzmann <niccokunzmann@rambler.ru>
2018-10-03 22:17:50 +0200

    translate offline map areas
    - fixes https://github.com/niccokunzmann/mundraub-android/issues/174

--- app/src/main/assets/map/translations/en.js
+++ app/src/main/assets/map/translations/en.js
@@ -3,6 +3,7 @@ define({
     "Plants": "Plants",
     "Satellite": "Satellite",
     "GPS": "GPS",
-    "Distance to marker": "Distance to marker"
+    "Distance to marker": "Distance to marker",
+    "Offline Map Areas": "Offline Map Areas"
 }); // do not put a comma at the end of the last translation
 
