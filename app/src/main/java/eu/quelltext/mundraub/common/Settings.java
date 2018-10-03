14c0f7c725cdb21b8a060ad4b214d9597ab15130
Travis-CI <mundraub-android@travis-ci.org>
2018-10-03 00:53:30 +0000

    Version v1.125
    - 55d0d2a show cache images of plants
        - fixes https://github.com/niccokunzmann/mundraub-android/issues/153
    - 9466328 choose offline area from settings
        - contributes to https://github.com/niccokunzmann/mundraub-android/issues/111

--- app/src/main/java/eu/quelltext/mundraub/common/Settings.java
+++ app/src/main/java/eu/quelltext/mundraub/common/Settings.java
@@ -57,7 +57,7 @@ public class Settings {
 
     private static final String PLANT_STORAGE_DIRECTORY_NAME = "eu.quelltext.mundraub";
     public static final String INVALID_HASH = "0000000000000000000000000000000000000000";
-    public static final String COMMIT_HASH = INVALID_HASH;
+    public static final String COMMIT_HASH = "0958f9f188559d6ebd9555de1a5e950e06aa8492";
     private static final String PERMISSION_PREFIX = "askForPermission_";
     public static int COMMIT_SUCCESSFUL = ChangeListener.SETTINGS_CAN_CHANGE;
 
