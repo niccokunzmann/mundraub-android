072a77bcf9807fd23ab4d56e49a4708962dc4f4f
Travis-CI <mundraub-android@travis-ci.org>
2018-10-03 17:14:52 +0000

    Version v1.126
    - 492cc23 create bounding box collection
        - contributes to https://github.com/niccokunzmann/mundraub-android/issues/111

--- app/src/main/java/eu/quelltext/mundraub/common/Settings.java
+++ app/src/main/java/eu/quelltext/mundraub/common/Settings.java
@@ -56,7 +56,7 @@ public class Settings {
 
     private static final String PLANT_STORAGE_DIRECTORY_NAME = "eu.quelltext.mundraub";
     public static final String INVALID_HASH = "0000000000000000000000000000000000000000";
-    public static final String COMMIT_HASH = INVALID_HASH;
+    public static final String COMMIT_HASH = "7225f23e16d13a4845742c98418d65d2c6c6732c";
     private static final String PERMISSION_PREFIX = "askForPermission_";
     public static int COMMIT_SUCCESSFUL = ChangeListener.SETTINGS_CAN_CHANGE;
 
