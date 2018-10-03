7ea2be83c6e56fc35a67ea0a445a31510ab8b61d
Travis-CI <mundraub-android@travis-ci.org>
2018-10-03 20:44:40 +0000

    Version v1.131
    - bfc4d1b allow viewing areas
        - fixes https://github.com/niccokunzmann/mundraub-android/issues/175

--- app/src/main/java/eu/quelltext/mundraub/common/Settings.java
+++ app/src/main/java/eu/quelltext/mundraub/common/Settings.java
@@ -57,7 +57,7 @@ public class Settings {
 
     private static final String PLANT_STORAGE_DIRECTORY_NAME = "eu.quelltext.mundraub";
     public static final String INVALID_HASH = "0000000000000000000000000000000000000000";
-    public static final String COMMIT_HASH = INVALID_HASH;
+    public static final String COMMIT_HASH = "fb44e0ff6a3e2d5a833765df1cf8759554237f37";
     private static final String PERMISSION_PREFIX = "askForPermission_";
     public static int COMMIT_SUCCESSFUL = ChangeListener.SETTINGS_CAN_CHANGE;
 
