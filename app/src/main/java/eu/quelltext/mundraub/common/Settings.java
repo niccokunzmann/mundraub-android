b719818fc112e7f1823b1a656db65c18c2812c87
Travis-CI <mundraub-android@travis-ci.org>
2018-10-10 13:52:28 +0000

    Version v1.140
    - f1d82c4 check if plants are nearby
        - fixes https://github.com/niccokunzmann/mundraub-android/issues/95

--- app/src/main/java/eu/quelltext/mundraub/common/Settings.java
+++ app/src/main/java/eu/quelltext/mundraub/common/Settings.java
@@ -57,7 +57,7 @@ public class Settings {
 
     private static final String PLANT_STORAGE_DIRECTORY_NAME = "eu.quelltext.mundraub";
     public static final String INVALID_HASH = "0000000000000000000000000000000000000000";
-    public static final String COMMIT_HASH = INVALID_HASH;
+    public static final String COMMIT_HASH = "ad4d4fe5c57be9286ce368858ec7d4be173e097d";
     private static final String PERMISSION_PREFIX = "askForPermission_";
     public static int COMMIT_SUCCESSFUL = ChangeListener.SETTINGS_CAN_CHANGE;
 
