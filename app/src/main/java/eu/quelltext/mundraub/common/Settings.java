afed3a3708ce7d68d4c003f8c73328ddfd1fbff7
Travis-CI <mundraub-android@travis-ci.org>
2018-10-10 09:18:38 +0000

    Version v1.138
    - c71e947 show progress bar without color
        - fixes https://github.com/niccokunzmann/mundraub-android/issues/188

--- app/src/main/java/eu/quelltext/mundraub/common/Settings.java
+++ app/src/main/java/eu/quelltext/mundraub/common/Settings.java
@@ -57,7 +57,7 @@ public class Settings {
 
     private static final String PLANT_STORAGE_DIRECTORY_NAME = "eu.quelltext.mundraub";
     public static final String INVALID_HASH = "0000000000000000000000000000000000000000";
-    public static final String COMMIT_HASH = INVALID_HASH;
+    public static final String COMMIT_HASH = "a67fb0e36746d31bac9a6bfd93481153394be314";
     private static final String PERMISSION_PREFIX = "askForPermission_";
     public static int COMMIT_SUCCESSFUL = ChangeListener.SETTINGS_CAN_CHANGE;
 
