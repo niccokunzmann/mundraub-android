e0206447683b82c5c6792d8782657b10d53e3d07
Travis-CI <mundraub-android@travis-ci.org>
2018-10-12 12:09:50 +0000

    Version v1.143
    - d46823f create style guide
        - fixes https://github.com/niccokunzmann/mundraub-android/issues/194
    - 85f2e72 make documentation consistent
        - fixes https://github.com/niccokunzmann/mundraub-android/issues/196
    - e6fd1ef use metadata directory for f-droid description and screenshots
        - contributes to https://github.com/niccokunzmann/mundraub-android/issues/69

--- app/src/main/java/eu/quelltext/mundraub/common/Settings.java
+++ app/src/main/java/eu/quelltext/mundraub/common/Settings.java
@@ -57,7 +57,7 @@ public class Settings {
 
     private static final String PLANT_STORAGE_DIRECTORY_NAME = "eu.quelltext.mundraub";
     public static final String INVALID_HASH = "0000000000000000000000000000000000000000";
-    public static final String COMMIT_HASH = INVALID_HASH;
+    public static final String COMMIT_HASH = "8d3d57d3e29c8ee4bf86f7e561c37e88c4830f2e";
     private static final String PERMISSION_PREFIX = "askForPermission_";
     public static int COMMIT_SUCCESSFUL = ChangeListener.SETTINGS_CAN_CHANGE;
 
