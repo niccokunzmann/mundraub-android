9c5db4bbbadda5d8875e66aabbc7d0d633c91648
Travis-CI <mundraub-android@travis-ci.org>
2018-10-14 19:45:53 +0000

    Version v1.144
    - 06a4cd9 add launcher rounded icons.
        fixes #192

--- app/src/main/java/eu/quelltext/mundraub/common/Settings.java
+++ app/src/main/java/eu/quelltext/mundraub/common/Settings.java
@@ -57,7 +57,7 @@ public class Settings {
 
     private static final String PLANT_STORAGE_DIRECTORY_NAME = "eu.quelltext.mundraub";
     public static final String INVALID_HASH = "0000000000000000000000000000000000000000";
-    public static final String COMMIT_HASH = INVALID_HASH;
+    public static final String COMMIT_HASH = "e7a44114b502d3923ae612c9221a61f3b5743d46";
     private static final String PERMISSION_PREFIX = "askForPermission_";
     public static int COMMIT_SUCCESSFUL = ChangeListener.SETTINGS_CAN_CHANGE;
 
