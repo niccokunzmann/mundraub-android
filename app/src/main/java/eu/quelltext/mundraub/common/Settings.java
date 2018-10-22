334e74407151a7fe19e0f0de362076b81cf0355c
Travis-CI <mundraub-android@travis-ci.org>
2018-10-22 20:18:22 +0000

    Version v1.157
    - 99e2323 open a dialog for the privacy policy on first start
        - fixes https://github.com/niccokunzmann/mundraub-android/issues/205
    - 8d690e5 save whether the user has read the policy in the settings
        - contributes to https://github.com/niccokunzmann/mundraub-android/issues/205
    - 90c57a0 embed privacy policy in app
        - contributes to https://github.com/niccokunzmann/mundraub-android/issues/205

--- app/src/main/java/eu/quelltext/mundraub/common/Settings.java
+++ app/src/main/java/eu/quelltext/mundraub/common/Settings.java
@@ -57,7 +57,7 @@ public class Settings {
 
     private static final String PLANT_STORAGE_DIRECTORY_NAME = "eu.quelltext.mundraub";
     public static final String INVALID_HASH = "0000000000000000000000000000000000000000";
-    public static final String COMMIT_HASH = INVALID_HASH;
+    public static final String COMMIT_HASH = "3859e9df12af427ee2c07c1d77e8672fe6813b16";
     private static final String PERMISSION_PREFIX = "askForPermission_";
     public static int COMMIT_SUCCESSFUL = ChangeListener.SETTINGS_CAN_CHANGE;
 
