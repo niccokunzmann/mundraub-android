b265a6e94b9193fa139724cf7ce1b6959de4048e
Nicco Kunzmann <niccokunzmann@rambler.ru>
2018-10-23 16:43:16 +0200

    correct comment

--- app/src/main/java/eu/quelltext/mundraub/activities/WebViewBaseActivity.java
+++ app/src/main/java/eu/quelltext/mundraub/activities/WebViewBaseActivity.java
@@ -31,7 +31,7 @@ public class WebViewBaseActivity extends MundraubBaseActivity {
         webSettings.setDatabaseEnabled(true); // https://stackoverflow.com/a/8921072
         webSettings.setDomStorageEnabled(true); // https://stackoverflow.com/a/8921072
         webSettings.setGeolocationEnabled(true); // from https://stackoverflow.com/a/43384409/1320237
-        webSettings.setUserAgentString(webSettings.getUserAgentString() + " | language: " + Locale.getDefault().getLanguage()); // passthe user language from https://stackoverflow.com/a/9380140/1320237
+        webSettings.setUserAgentString(webSettings.getUserAgentString() + " | language: " + Locale.getDefault().getLanguage()); // pass the user language from https://stackoverflow.com/a/9380140/1320237
 
         webView.setWebChromeClient(new WebChromeClient() {
             @Override
