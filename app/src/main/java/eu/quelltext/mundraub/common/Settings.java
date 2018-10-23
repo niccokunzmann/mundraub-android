a74b680e313cb9d3d8db3345486dc829eb98282e
Travis-CI <mundraub-android@travis-ci.org>
2018-10-23 16:27:57 +0000

    Version v1.158
    - 3b2ed79 switch privacy policy depending on language
        - fixes https://github.com/niccokunzmann/mundraub-android/issues/204
    - 2f5c8be add privacy policy to transifex
        - contributes to https://github.com/niccokunzmann/mundraub-android/issues/204

--- app/src/main/java/eu/quelltext/mundraub/common/Settings.java
+++ app/src/main/java/eu/quelltext/mundraub/common/Settings.java
@@ -57,7 +57,7 @@ public class Settings {
 
     private static final String PLANT_STORAGE_DIRECTORY_NAME = "eu.quelltext.mundraub";
     public static final String INVALID_HASH = "0000000000000000000000000000000000000000";
-    public static final String COMMIT_HASH = INVALID_HASH;
+    public static final String COMMIT_HASH = "be2bd0e222518dc62d3379542e4adc377a087d99";
     private static final String PERMISSION_PREFIX = "askForPermission_";
     public static int COMMIT_SUCCESSFUL = ChangeListener.SETTINGS_CAN_CHANGE;
 
