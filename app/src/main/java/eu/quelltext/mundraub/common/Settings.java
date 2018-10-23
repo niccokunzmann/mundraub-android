da03751a569301f392734b93941f1b2e01463742
Travis-CI <mundraub-android@travis-ci.org>
2018-10-23 19:03:48 +0000

    Version v1.159
    - b2d33c1 allow fetching translations with country codes
        - contributes to https://github.com/niccokunzmann/mundraub-android/issues/212
    - 277897a release privacy policy
        - contributes to https://github.com/niccokunzmann/mundraub-android/issues/205

--- app/src/main/java/eu/quelltext/mundraub/common/Settings.java
+++ app/src/main/java/eu/quelltext/mundraub/common/Settings.java
@@ -57,7 +57,7 @@ public class Settings {
 
     private static final String PLANT_STORAGE_DIRECTORY_NAME = "eu.quelltext.mundraub";
     public static final String INVALID_HASH = "0000000000000000000000000000000000000000";
-    public static final String COMMIT_HASH = INVALID_HASH;
+    public static final String COMMIT_HASH = "89fa80557d8af86430ded256f803efeeaa515214";
     private static final String PERMISSION_PREFIX = "askForPermission_";
     public static int COMMIT_SUCCESSFUL = ChangeListener.SETTINGS_CAN_CHANGE;
 
