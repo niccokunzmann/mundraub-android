3b2ed79f8ea7a02f6df6b06a2d23a0228e4cffcd
Nicco Kunzmann <niccokunzmann@rambler.ru>
2018-10-23 16:45:50 +0200

    switch privacy policy depending on language
    - open the privacy policy if translated
    - English is default
    - fixes https://github.com/niccokunzmann/mundraub-android/issues/204

--- app/src/main/java/eu/quelltext/mundraub/activities/PrivacyPolicyActivity.java
+++ app/src/main/java/eu/quelltext/mundraub/activities/PrivacyPolicyActivity.java
@@ -1,19 +1,43 @@
 package eu.quelltext.mundraub.activities;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
 
 import eu.quelltext.mundraub.R;
 import eu.quelltext.mundraub.common.Settings;
 
 public class PrivacyPolicyActivity extends WebViewBaseActivity {
 
+    private static final String PRIVACY_FOLDER = "privacy";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_privacy_policy);
         initializeWebView(R.id.web_view);
-        webView.loadUrl("file:///android_asset/privacy/en.html");
+        webView.loadUrl(getUrl());
         Settings.setUserHasReadThePrivacyPolicy();
+    }
+
+    @NonNull
+    private String getUrl() {
+        String fileName = "en.html";
+        try {
+            List<String> files = Arrays.asList(getAssets().list(PRIVACY_FOLDER));
+            String language = Locale.getDefault().getLanguage();
+            String translatedFileName = language + ".html";
+            if (files.contains(translatedFileName)) {
+                fileName = translatedFileName;
+            }
+        } catch (IOException e) {
+            log.printStackTrace(e);
+        }
+        return "file:///android_asset/privacy/" + fileName;
     }
 
     @Override
