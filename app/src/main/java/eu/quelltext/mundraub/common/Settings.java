a66ded2cb423a0ee7d4bd8bdf3021f06f042adc4
Travis-CI <mundraub-android@travis-ci.org>
2018-11-01 21:53:08 +0000

    Version v1.164
    - 47acd70 Changing map conrols from dark blue to app color (yellow)
        - [CSS] Use app colors for the map controls #197

--- app/src/main/java/eu/quelltext/mundraub/common/Settings.java
+++ app/src/main/java/eu/quelltext/mundraub/common/Settings.java
@@ -57,7 +57,7 @@ public class Settings {
 
     private static final String PLANT_STORAGE_DIRECTORY_NAME = "eu.quelltext.mundraub";
     public static final String INVALID_HASH = "0000000000000000000000000000000000000000";
-    public static final String COMMIT_HASH = INVALID_HASH;
+    public static final String COMMIT_HASH = "fa3b9c05244da653f1fb4845f23e8ce46c2940a2";
     private static final String PERMISSION_PREFIX = "askForPermission_";
     public static int COMMIT_SUCCESSFUL = ChangeListener.SETTINGS_CAN_CHANGE;
 
