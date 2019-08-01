3b8f226b4341bfc4bf827c04b3720c6ba4db5819
Travis-CI <mundraub-android@travis-ci.org>
2019-08-01 13:01:33 +0000

    Version v1.174
    - cb484ff Merge pull request #237 from niccokunzmann/changelog
        Merge pull request #237 from niccokunzmann/changelog
    - 00dfdc7 add changelog build to travis build
        - fixes https://github.com/niccokunzmann/mundraub-android/issues/182
    - 9656965 generate changelog file
        - contributes to https://github.com/niccokunzmann/mundraub-android/issues/182

--- app/src/main/java/eu/quelltext/mundraub/common/Settings.java
+++ app/src/main/java/eu/quelltext/mundraub/common/Settings.java
@@ -57,7 +57,7 @@ public class Settings {
 
     private static final String PLANT_STORAGE_DIRECTORY_NAME = "eu.quelltext.mundraub";
     public static final String INVALID_HASH = "0000000000000000000000000000000000000000";
-    public static final String COMMIT_HASH = INVALID_HASH;
+    public static final String COMMIT_HASH = "27cfaeca8cede30b05b070576eb14202c7ae6917";
     private static final String PERMISSION_PREFIX = "askForPermission_";
     public static int COMMIT_SUCCESSFUL = ChangeListener.SETTINGS_CAN_CHANGE;
 
