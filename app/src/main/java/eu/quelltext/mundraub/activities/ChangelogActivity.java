172518760c0d9d4e3d262e7ec8a0d61c5aa2ce44
Nicco Kunzmann <niccokunzmann@rambler.ru>
2019-08-01 14:26:46 +0200

    add changelog activity to menu

--- app/src/main/java/eu/quelltext/mundraub/activities/ChangelogActivity.java
+++ app/src/main/java/eu/quelltext/mundraub/activities/ChangelogActivity.java
@@ -1,4 +1,12 @@
 package eu.quelltext.mundraub.activities;
 
 public class ChangelogActivity extends LocalizedWebViewActivity {
+    @Override
+    public String folderName() {
+        return "changelog";
+    }
+
+    @Override
+    protected void menuOpenChangelog() {
+    }
 }
