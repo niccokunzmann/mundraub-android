9a5ad6dab3626b77232e2e9571004bc625445843
Nicco Kunzmann <niccokunzmann@rambler.ru>
2019-08-02 15:09:04 +0200

    add copyright to data

--- app/src/main/java/eu/quelltext/mundraub/activities/AddressSearchActivity.java
+++ app/src/main/java/eu/quelltext/mundraub/activities/AddressSearchActivity.java
@@ -1,17 +1,28 @@
 package eu.quelltext.mundraub.activities;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 import eu.quelltext.mundraub.R;
 import eu.quelltext.mundraub.activities.dummy.DummyContent;
 
-public class AddressSearchActivity extends AppCompatActivity implements AddressSearchResultFragment.OnListFragmentInteractionListener {
+public class AddressSearchActivity extends MundraubBaseActivity implements AddressSearchResultFragment.OnListFragmentInteractionListener {
+
+    private static final String OPEN_STREET_MAP_COPYRIGHT_URL = "https://osm.org/copyright";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_address_search);
+
+        TextView searchLicense = (TextView) findViewById(R.id.text_seach_license);
+        searchLicense.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openURLInBrowser(OPEN_STREET_MAP_COPYRIGHT_URL);
+            }
+        });
     }
 
     @Override
