715129860dfeb997b6bf389da8073733132b381d
Nicco Kunzmann <niccokunzmann@rambler.ru>
2019-08-02 07:20:46 +0200

    add address search activity

---
+++ app/src/main/java/eu/quelltext/mundraub/activities/AddressSearchActivity.java
@@ -0,0 +1,15 @@
+package eu.quelltext.mundraub.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import eu.quelltext.mundraub.R;
+
+public class AddressSearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_address_search);
+    }
+}
