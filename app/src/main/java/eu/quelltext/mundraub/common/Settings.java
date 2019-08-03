0b247e61d5d680347b07251f19bdac5845cedfc3
Travis-CI <mundraub-android@travis-ci.org>
2019-08-03 23:58:48 +0000

    Version v1.177
    - 574e15e Merge pull request #242 from niccokunzmann/address-search
        Merge pull request #242 from niccokunzmann/address-search
    - 969e1e9 open map at found places
        - fixes https://github.com/niccokunzmann/mundraub-android/issues/128

--- app/src/main/java/eu/quelltext/mundraub/common/Settings.java
+++ app/src/main/java/eu/quelltext/mundraub/common/Settings.java
@@ -57,7 +57,7 @@ public class Settings {
 
     private static final String PLANT_STORAGE_DIRECTORY_NAME = "eu.quelltext.mundraub";
     public static final String INVALID_HASH = "0000000000000000000000000000000000000000";
-    public static final String COMMIT_HASH = INVALID_HASH;
+    public static final String COMMIT_HASH = "c4021ee978fae4c1ad695d640bf980f1a70888dc";
     private static final String PERMISSION_PREFIX = "askForPermission_";
     public static int COMMIT_SUCCESSFUL = ChangeListener.SETTINGS_CAN_CHANGE;
 
