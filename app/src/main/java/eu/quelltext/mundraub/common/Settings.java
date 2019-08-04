b10482b5af457488df391e7e92d79e22c7d31531
Travis-CI <mundraub-android@travis-ci.org>
2019-08-04 09:42:29 +0000

    Version v1.179
    - b4f05c8 Merge pull request #246 from niccokunzmann/zoom-map-on-search
        Merge pull request #246 from niccokunzmann/zoom-map-on-search
    - 87aebdb comunicate nominatim url to map through extent in url
        - fixes https://github.com/niccokunzmann/mundraub-android/issues/245

--- app/src/main/java/eu/quelltext/mundraub/common/Settings.java
+++ app/src/main/java/eu/quelltext/mundraub/common/Settings.java
@@ -57,7 +57,7 @@ public class Settings {
 
     private static final String PLANT_STORAGE_DIRECTORY_NAME = "eu.quelltext.mundraub";
     public static final String INVALID_HASH = "0000000000000000000000000000000000000000";
-    public static final String COMMIT_HASH = INVALID_HASH;
+    public static final String COMMIT_HASH = "1465fe72794ba3c04ba0248596cb6956642ec0de";
     private static final String PERMISSION_PREFIX = "askForPermission_";
     public static int COMMIT_SUCCESSFUL = ChangeListener.SETTINGS_CAN_CHANGE;
 
