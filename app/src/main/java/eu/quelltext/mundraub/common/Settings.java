90e9ee39d9100360e79026701766977187bf0dee
Travis-CI <mundraub-android@travis-ci.org>
2019-08-05 12:36:05 +0000

    Version v1.181
    - 3ab24c2 remove duplicates in search storage
        - fixes https://github.com/niccokunzmann/mundraub-android/issues/247

--- app/src/main/java/eu/quelltext/mundraub/common/Settings.java
+++ app/src/main/java/eu/quelltext/mundraub/common/Settings.java
@@ -57,7 +57,7 @@ public class Settings {
 
     private static final String PLANT_STORAGE_DIRECTORY_NAME = "eu.quelltext.mundraub";
     public static final String INVALID_HASH = "0000000000000000000000000000000000000000";
-    public static final String COMMIT_HASH = INVALID_HASH;
+    public static final String COMMIT_HASH = "c40fea0b4c0edf2d1d69f0186bcee8ff66d6e910";
     private static final String PERMISSION_PREFIX = "askForPermission_";
     public static int COMMIT_SUCCESSFUL = ChangeListener.SETTINGS_CAN_CHANGE;
 
