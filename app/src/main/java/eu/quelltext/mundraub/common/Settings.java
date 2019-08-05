d2ea1cd74c4d616f1f5fe9645527c1200624832f
Travis-CI <mundraub-android@travis-ci.org>
2019-08-05 21:26:03 +0000

    Version v1.185
    - 9fa706f create google play release files, signed if possible
        - related https://github.com/niccokunzmann/mundraub-android/issues/203

--- app/src/main/java/eu/quelltext/mundraub/common/Settings.java
+++ app/src/main/java/eu/quelltext/mundraub/common/Settings.java
@@ -57,7 +57,7 @@ public class Settings {
 
     private static final String PLANT_STORAGE_DIRECTORY_NAME = "eu.quelltext.mundraub";
     public static final String INVALID_HASH = "0000000000000000000000000000000000000000";
-    public static final String COMMIT_HASH = INVALID_HASH;
+    public static final String COMMIT_HASH = "37711cf0ec33ada65f0cdb9a11337cf6d35e766b";
     private static final String PERMISSION_PREFIX = "askForPermission_";
     public static int COMMIT_SUCCESSFUL = ChangeListener.SETTINGS_CAN_CHANGE;
 
