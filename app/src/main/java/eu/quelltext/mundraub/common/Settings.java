02542f438518f2f63f90d160759d5e06998e20d9
Travis-CI <mundraub-android@travis-ci.org>
2019-08-05 17:07:17 +0000

    Version v1.184
    - 05e4d35 Merge pull request #249 from niccokunzmann/sign-releases
        Merge pull request #249 from niccokunzmann/sign-releases

--- app/src/main/java/eu/quelltext/mundraub/common/Settings.java
+++ app/src/main/java/eu/quelltext/mundraub/common/Settings.java
@@ -57,7 +57,7 @@ public class Settings {
 
     private static final String PLANT_STORAGE_DIRECTORY_NAME = "eu.quelltext.mundraub";
     public static final String INVALID_HASH = "0000000000000000000000000000000000000000";
-    public static final String COMMIT_HASH = INVALID_HASH;
+    public static final String COMMIT_HASH = "8f048ef8f3092e524e0365bc25e9aa37d7e572ac";
     private static final String PERMISSION_PREFIX = "askForPermission_";
     public static int COMMIT_SUCCESSFUL = ChangeListener.SETTINGS_CAN_CHANGE;
 
