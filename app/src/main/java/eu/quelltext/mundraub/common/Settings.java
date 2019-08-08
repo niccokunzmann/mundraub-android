36e1815318636c07a1c2feefeca5ba86371fca6e
Travis-CI <mundraub-android@travis-ci.org>
2019-08-08 07:19:19 +0000

    Version v1.197
    - e038e8a add English phone screenshots
        - fixes https://github.com/niccokunzmann/mundraub-android/issues/210

--- app/src/main/java/eu/quelltext/mundraub/common/Settings.java
+++ app/src/main/java/eu/quelltext/mundraub/common/Settings.java
@@ -57,7 +57,7 @@ public class Settings {
 
     private static final String PLANT_STORAGE_DIRECTORY_NAME = "eu.quelltext.mundraub";
     public static final String INVALID_HASH = "0000000000000000000000000000000000000000";
-    public static final String COMMIT_HASH = INVALID_HASH;
+    public static final String COMMIT_HASH = "1f525d748a1f6e1a69484f184216ff62bc36b120";
     private static final String PERMISSION_PREFIX = "askForPermission_";
     public static int COMMIT_SUCCESSFUL = ChangeListener.SETTINGS_CAN_CHANGE;
 
