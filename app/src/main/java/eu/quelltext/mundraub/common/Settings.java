2bff0d0e3f822f9cad13488377dd069e8c14f068
Travis-CI <mundraub-android@travis-ci.org>
2019-08-14 06:27:38 +0000

    Version v1.202

--- app/src/main/java/eu/quelltext/mundraub/common/Settings.java
+++ app/src/main/java/eu/quelltext/mundraub/common/Settings.java
@@ -57,7 +57,7 @@ public class Settings {
 
     private static final String PLANT_STORAGE_DIRECTORY_NAME = "eu.quelltext.mundraub";
     public static final String INVALID_HASH = "0000000000000000000000000000000000000000";
-    public static final String COMMIT_HASH = INVALID_HASH;
+    public static final String COMMIT_HASH = "6865a5bea3f35b0cc10ae54adb9f447df048799b";
     private static final String PERMISSION_PREFIX = "askForPermission_";
     public static int COMMIT_SUCCESSFUL = ChangeListener.SETTINGS_CAN_CHANGE;
 
