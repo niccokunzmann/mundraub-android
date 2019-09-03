905623212faa58100630ee04a18b654df2812645
Travis-CI <mundraub-android@travis-ci.org>
2019-09-03 05:51:58 +0000

    v1.214: update languages

---
+++ app/src/main/assets/map/translations/cs.js
@@ -0,0 +1,9 @@
+define(
+{
+    "Location": "Location",
+    "Plants": "Plants",
+    "Satellite": "Satellite",
+    "GPS": "GPS",
+    "Distance to marker": "Distance to marker",
+    "Offline Map Areas": "Offline Map Areas"
+});
