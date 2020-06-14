4dec1adb116a31183a3799e7540c0f56873c0fc2
Travis-CI <mundraub-android@travis-ci.org>
2020-06-14 21:19:58 +0000

    v1.231: update languages

--- app/src/main/assets/map/translations/de_AT.js
+++ app/src/main/assets/map/translations/de_AT.js
@@ -3,7 +3,7 @@ define(
     "Location": "Position",
     "Plants": "Pflanzen",
     "Satellite": "Satellit",
-    "Street Map": "Straßenkarte",
+    "Street Map": "Street Map",
     "GPS": "GPS",
     "Distance to marker": "Entfernung zur Markierung",
     "Offline Map Areas": "Offline-Kartengebiete",
