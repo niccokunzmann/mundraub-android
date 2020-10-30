46009b8a1a1b176a6fb15a693113b73e32b2d96b
Travis-CI <mundraub-android@travis-ci.org>
2020-10-30 13:40:23 +0000

    v1.234: update languages

---
+++ app/src/main/assets/map/translations/cs.js
@@ -0,0 +1,11 @@
+define(
+{
+    "Location": "Lokace",
+    "Plants": "Rostliny",
+    "Satellite": "Satelitní",
+    "Street Map": "Mapa ulic",
+    "GPS": "GPS",
+    "Distance to marker": "Vzdálenost ke značce",
+    "Offline Map Areas": "Mapové oblasti offline",
+    "My Plants": "Moje rostliny"
+});
---
+++ app/src/main/assets/map/translations/eu.js
@@ -0,0 +1,11 @@
+define(
+{
+    "Location": "Kokapena",
+    "Plants": "Landareak",
+    "Satellite": "Satelitea",
+    "Street Map": "Street Map",
+    "GPS": "GPS",
+    "Distance to marker": "Distantzia markatzailera",
+    "Offline Map Areas": "Lineaz kanpoko mapa guneak",
+    "My Plants": "Nire landareak"
+});
