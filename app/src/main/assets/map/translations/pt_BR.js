9f2eb338c3dced53443f26de09576f4f546e9358
Travis-CI <mundraub-android@travis-ci.org>
2021-02-21 12:02:34 +0000

    v1.236: update languages

---
+++ app/src/main/assets/map/translations/pt_BR.js
@@ -0,0 +1,11 @@
+define(
+{
+    "Location": "Localização",
+    "Plants": "Plantas",
+    "Satellite": "Satélite",
+    "Street Map": "Mapa das Ruas",
+    "GPS": "GPS",
+    "Distance to marker": "Distância até o marcador",
+    "Offline Map Areas": "Áreas de Mapa Offline",
+    "My Plants": "Minhas Plantas"
+});
